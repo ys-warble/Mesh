c46b67cd4d42284d5d9fd47f66583fd427b1bd61
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-25 01:00:15 -0600

    To add more definition on Space

--- WarbleSimulation/System/Space.py
+++ WarbleSimulation/System/Space.py
@@ -1,44 +1,57 @@
-from enum import Enum
-
-
-class SpaceFactor(Enum):
-    MATTER = 'MATTER'
-    TEMPERATURE = 'TEMPERATURE'
-    HUMIDITY = 'HUMIDITY'
-    LUMINOSITY = 'LUMINOSITY'
-    AIR_MOVEMENT = 'AIR_MOVEMENT'
+import WarbleSimulation.System.SpaceFactor as SpaceFactor
 
 
 class Space:
-    def __init__(self, name, x, y, z, space_factors, resolution=1):
+    def __init__(self, name, dimension, space_factor_types, resolution=1):
         if isinstance(name, str):
             self.name = name
         else:
             raise TypeError
 
-        if isinstance(x, int) and isinstance(y, int) and isinstance(z, int) and isinstance(resolution, int):
-            self.x = x
-            self.y = y
-            self.z = z
+        if isinstance(dimension, tuple) \
+                and len(dimension) == 3 \
+                and all(isinstance(d, int) for d in dimension) \
+                and isinstance(resolution, int):
+            self.dimension = dimension
             self.resolution = resolution
+        elif len(dimension) != 3:
+            raise IndexError
         else:
             raise TypeError
 
         self.space_factors = {}
-        if isinstance(space_factors, list) and all(isinstance(sf, SpaceFactor) for sf in space_factors):
-            for space_factor in space_factors:
-                # TODO: should not be None
-                self.space_factors[space_factor] = None
+        self.add_space_factor(space_factor_types)
+
+    def add_space_factor(self, space_factor_type):
+        space_factor_type = [space_factor_type] if isinstance(space_factor_type,
+                                                              SpaceFactor.SpaceFactor) else space_factor_type
+
+        if isinstance(space_factor_type, list) and all(
+                isinstance(sf, SpaceFactor.SpaceFactor) for sf in space_factor_type):
+            for sf in space_factor_type:
+                self.space_factors[sf] = SpaceFactor.generate(sf, self.dimension, self.resolution)
         else:
             raise TypeError
 
-    def add_space_factor(self, space_factor):
-        if isinstance(space_factor, SpaceFactor):
-            # TODO: should not be None
-            self.space_factors[space_factor] = None
-        elif isinstance(space_factor, list) and all(isinstance(sf, SpaceFactor) for sf in space_factor):
-            for sf in space_factor:
-                # TODO: should not be None
-                self.space_factors[sf] = None
+    def __str__(self):
+        string = ''
+        string += 'Space'
+        string += '('
+        string += '\n  name=%s,' % self.name
+        string += '\n  dimension=%s,' % str(self.dimension)
+        string += '\n  resolution=%s,' % str(self.resolution)
+
+        temp1 = []
+        for a, b in self.space_factors.items():
+            temp2 = []
+            for c, d in b.items():
+                temp2.append('%s:%s' % (str(c), d.shape))
+            temp1.append('%s:{\n      ' % a + ',\n      '.join(temp2) + ']')
+        if len(temp1) > 0:
+            string_space_factors = '{\n    ' + ',\n    '.join(temp1) + '\n  }'
         else:
-            raise TypeError
+            string_space_factors = '{}'
+
+        string += '\n  space_factors=%s' % string_space_factors
+        string += '\n)'
+        return string
---
+++ WarbleSimulation/System/SpaceFactor.py
@@ -0,0 +1,87 @@
+from enum import Enum
+
+import numpy as np
+
+
+class SpaceFactor(Enum):
+    MATTER = 'MATTER'
+    TEMPERATURE = 'TEMPERATURE'
+    HUMIDITY = 'HUMIDITY'
+    LUMINOSITY = 'LUMINOSITY'
+    AIR_MOVEMENT = 'AIR_MOVEMENT'
+
+
+class Matter(Enum):
+    MATTER = 0
+
+
+class MatterType(Enum):
+    ETHER = 0
+    ATMOSPHERE = 1
+    WATER = 2
+    WOOD = 3
+    CONCRETE = 4
+    SOLID = 5
+
+
+class Temperature(Enum):
+    TEMPERATURE = 0  # in Kelvin(K)
+
+
+class Humidity(Enum):
+    HUMIDITY = 0  # in percentage (%)
+
+
+class Luminosity(Enum):
+    HUE = 1  # range 0-255
+    SATURATION = 2  # range 0-255
+    BRIGHTNESS = 3  # range 0-255
+
+
+class AirMovement(Enum):
+    X = 1  # in m/s
+    Y = 2  # in m/s
+    Z = 3  # in m/s
+
+
+def generate(space_factor_type, dimension, resolution):
+    '''
+
+    :param space_factor_type: SpaceFactor Enum
+    :param dimension: Tuple dimensions o
+    :param resolution:
+    :return:
+    '''
+
+    space_subfactors = []
+    dtypes = []
+    if space_factor_type == SpaceFactor.MATTER:
+        space_subfactors = Matter
+        dtypes = [MatterType]
+
+    elif space_factor_type == SpaceFactor.TEMPERATURE:
+        space_subfactors = Temperature
+        dtypes = [int]
+
+    elif space_factor_type == SpaceFactor.HUMIDITY:
+        space_subfactors = Humidity
+        dtypes = [int]
+
+    elif space_factor_type == SpaceFactor.LUMINOSITY:
+        space_subfactors = Luminosity
+        dtypes = [int]
+
+    elif space_factor_type == SpaceFactor.AIR_MOVEMENT:
+        space_subfactors = AirMovement
+        dtypes = [int]
+    else:
+        raise Exception('Unknown SpaceFactor Type')
+
+    space_matrices = {}
+    for i, space_subfactor in enumerate(space_subfactors):
+        if i < len(dtypes) and dtypes[i] is not None:
+            space_matrices[space_subfactor] = np.zeros(tuple([i * resolution for i in dimension]), dtypes[i])
+        else:
+            space_matrices[space_subfactor] = np.zeros(tuple([i * resolution for i in dimension]))
+
+    return space_matrices
