91b3b6a9a241950ebb2243bad520036cb9508221
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-24 21:20:56 -0600

    Initial commit on class Entity, Space, System

---
+++ WarbleSimulation/System/Space.py
@@ -0,0 +1,41 @@
+from enum import Enum
+
+
+class SpaceFactor(Enum):
+    MATTER = 'MATTER'
+    TEMPERATURE = 'TEMPERATURE'
+    HUMIDITY = 'HUMIDITY'
+    LUMINOSITY = 'LUMINOSITY'
+    AIR_MOVEMENT = 'AIR_MOVEMENT'
+
+
+class Space:
+    def __init__(self, name, x, y, z, space_factors, resolution=1):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError
+
+        if isinstance(x, int) and isinstance(y, int) and isinstance(z, int) and isinstance(resolution, int):
+            self.x = x
+            self.y = y
+            self.z = z
+            self.resolution = resolution
+        else:
+            raise TypeError
+
+        self.space_factors = {}
+        if isinstance(space_factors, list) and all(isinstance(sf, SpaceFactor) for sf in space_factors):
+            for space_factor in space_factors:
+                self.space_factors[space_factor] = None
+        else:
+            raise TypeError
+
+    def add_space_factor(self, space_factor):
+        if isinstance(space_factor, SpaceFactor):
+            self.space_factors[space_factor] = None
+        elif isinstance(space_factor, list) and all(isinstance(sf, SpaceFactor) for sf in space_factor):
+            for sf in space_factor:
+                self.space_factors[sf] = None
+        else:
+            raise TypeError
---
+++ WarbleSimulation/System/System.py
@@ -0,0 +1,6 @@
+class System:
+    def __init__(self, name):
+        self.name = name
+
+        self.space = None
+        self.entities = None
