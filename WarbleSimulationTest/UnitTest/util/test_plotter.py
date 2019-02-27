15053c3dab25ad7a1468c1aad7414a1a33e1a50f
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-26 19:22:51 -0600

    To define test_plotter.py

---
+++ WarbleSimulationTest/UnitTest/util/test_plotter.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+import numpy as np
+
+from WarbleSimulation.util import Plotter
+
+
+class TestPlotter(TestCase):
+    def test_plotly_plot_scatter_3d(self):
+        dimension = (20, 10, 5)
+        resolution = 4
+
+        space_factor_matter = np.zeros(tuple([i * resolution for i in dimension]))
+
+        space_factor_matter[:, 0:10, 0:5] = 5
+        space_factor_matter[:, 30:40, 0:5] = 3
+
+        space_factor_matter[0:20, :, 15:20] = 4
+        space_factor_matter[60:80, :, 15:20] = 3
+
+        Plotter.plotly_plot_scatter_3d(space_factor_matter, 2)
