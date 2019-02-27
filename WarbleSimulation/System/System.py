2b762fd2842c0a71aa0b7b2579733d5af43bd2be
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-26 18:58:32 -0600

    To remove def init_normal_space. To init self.entities to an empty list. To add def remove_entity.

--- WarbleSimulation/System/System.py
+++ WarbleSimulation/System/System.py
@@ -1,5 +1,4 @@
-import WarbleSimulation.System.Space as Space
-import WarbleSimulation.System.SpaceFactor as SpaceFactor
+from WarbleSimulation.System.Space import Space
 
 
 class System:
@@ -7,7 +6,7 @@ class System:
         self.name = name
 
         self.space = None
-        self.entities = None
+        self.entities = []
 
     def put_space(self, dimension, resolution=1, space_factor_types=None):
         """
@@ -24,29 +23,13 @@ class System:
 
         # TODO: How if the space is re-put after putting entities?
 
-    def init_normal_space(self):
-        for space_factor in self.space.space_factors.keys():
-            for space_subfactor in self.space.space_factors[space_factor].keys():
-                value = 0
-
-                if space_factor == SpaceFactor.SpaceFactor.MATTER and space_subfactor == SpaceFactor.Matter.MATTER:
-                    value = SpaceFactor.MatterType.ATMOSPHERE
-                elif space_factor == SpaceFactor.SpaceFactor.TEMPERATURE and space_subfactor == SpaceFactor.Temperature.TEMPERATURE:
-                    value = 300
-                elif space_factor == SpaceFactor.SpaceFactor.HUMIDITY and space_subfactor == SpaceFactor.Humidity.HUMIDITY:
-                    value = 0
-                elif space_factor == SpaceFactor.SpaceFactor.LUMINOSITY and space_subfactor in [
-                    SpaceFactor.Luminosity.HUE, SpaceFactor.Luminosity.SATURATION, SpaceFactor.Luminosity.BRIGHTNESS]:
-                    value = 0
-                elif space_factor == SpaceFactor.SpaceFactor.AIR_MOVEMENT and space_subfactor in [
-                    SpaceFactor.AirMovement.X, SpaceFactor.AirMovement.Y, SpaceFactor.AirMovement.Z]:
-                    value = 0
-
-                self.space.init_space_factor(space_factor, space_subfactor, value)
-
-    def init_space(self):
-        # This should be replaced by reading the content of input file
-        self.init_normal_space()
-
     def put_entity(self, entity, location, direction):
-        raise NotImplementedError
+        # TODO check validity
+
+        # Matter placement
+        # self.space.space_factors[SpaceFactor.SpaceFactor.MATTER][SpaceFactor.Matter.MATTER]
+
+        self.entities.append((entity, location, direction))
+
+    def remove_entity(self):
+        pass
