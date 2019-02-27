982334f8d632e9829140072c8b2901a2018856c2
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-26 18:59:28 -0600

    To define constructor for Wall

--- WarbleSimulation/System/Entity/Wall.py
+++ WarbleSimulation/System/Entity/Wall.py
@@ -1,8 +1,11 @@
-from WarbleSimulation.System import Entity
+from WarbleSimulation.System.Entity import Entity
+from WarbleSimulation.System.SpaceFactor import MatterType
 
 
 class Wall(Entity):
     identifier = 'wall'
 
-    def __init__(self, UUID):
-        super().__init__(UUID)
+    def __init__(self, uuid, dimension=(1, 1, 1)):
+        super().__init__(uuid)
+        self.dimension = dimension
+        self.matter_type = MatterType.PERFECT_SOLID
