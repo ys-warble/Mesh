b1008e27bde71a5237ac3d248b927b6e83d72c3c
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-28 00:57:12 -0600

    To change the default logging level to INFO

--- WarbleSimulation/settings.py
+++ WarbleSimulation/settings.py
@@ -4,7 +4,7 @@ import logging
 # Logging
 from WarbleSimulation.util.PlotterTool import PlotterTool
 
-LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL = logging.INFO
 
 # Plotting
 PLOTTER_TOOL = PlotterTool.PLOTLY
