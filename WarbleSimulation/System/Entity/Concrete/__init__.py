b2ae142aca7e3cd09d838c4fe587f5097c1c9db0
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-28 02:59:26 -0600

    To define transform_shape() to transform the orientation of entity_shape

--- WarbleSimulation/System/Entity/Concrete/__init__.py
+++ WarbleSimulation/System/Entity/Concrete/__init__.py
@@ -3,6 +3,46 @@ import numpy as np
 from WarbleSimulation.System.Entity import Entity
 
 
+def transform_shape(entity_shape, from_direction, to_direction):
+    # TODO check validity of entity and to_direction
+
+    if from_direction == to_direction:
+        return entity_shape
+    else:
+        f_s = False
+        f = 0
+        if 1 in from_direction:
+            f = from_direction.index(1)
+            f_s = False
+        elif -1 in from_direction:
+            f = from_direction.index(-1)
+            f_s = True
+
+        t_s = False
+        t = 0
+        if 1 in to_direction:
+            t = to_direction.index(1)
+            t_s = False
+        elif -1 in to_direction:
+            t = to_direction.index(-1)
+            t_s = True
+
+        rot_k = 0
+        rot_dir = (0, 0)
+        if f_s is not t_s and f == t:
+            rot_k = 2
+            t = f + 1 if f < 2 else f - 1
+            rot_dir = (f, t)
+        elif f_s is not t_s:
+            rot_k = 1
+            rot_dir = (t, f)
+        else:
+            rot_k = 1
+            rot_dir = (f, t)
+
+        return np.rot90(entity_shape, rot_k, rot_dir)
+
+
 class Concrete(Entity):
     default_dimension = (1, 1, 1)
     default_direction = (0, 1, 0)
