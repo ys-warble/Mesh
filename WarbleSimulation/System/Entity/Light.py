24917d107dc869da7b0990160a0a3bcd21d15e13
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-27 21:27:31 -0600

    To fix the constructor of Light

--- WarbleSimulation/System/Entity/Light.py
+++ WarbleSimulation/System/Entity/Light.py
@@ -4,5 +4,6 @@ from WarbleSimulation.System import Entity
 class Light(Entity):
     identifier = 'light'
 
-    def __init__(self, UUID):
-        super().__init__(UUID)
+    def __init__(self, uuid):
+        super().__init__(uuid)
+        self.dimension = (1, 1, 1)
