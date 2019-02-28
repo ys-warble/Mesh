9816a8d2fb1a327a2ec512234fd3bc7a86e4840f
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-28 00:56:34 -0600

    To define the get_shape() in Light

--- WarbleSimulation/System/Entity/Concrete/Light.py
+++ WarbleSimulation/System/Entity/Concrete/Light.py
@@ -1,12 +1,34 @@
+import numpy as np
+
 from WarbleSimulation.System.Entity.Concrete import Concrete
 from WarbleSimulation.System.SpaceFactor import MatterType
 
 
 class Light(Concrete):
     identifier = 'light'
-    default_dimension = (1, 1, 1)
+    default_dimension = (3, 3, 3)
 
     def __init__(self, uuid, dimension_x=(1, 1, 1)):
-        super().__init__(uuid=uuid, dimension_x=dimension_x, matter_type=MatterType.SOLID)
+        super().__init__(uuid=uuid, dimension_x=dimension_x, matter_type=MatterType.GLASS)
         self.dimension = tuple(
             [Light.default_dimension[i] * self.dimension_x[i] for i in range(len(Light.default_dimension))])
+
+    def get_shape(self):
+        matter = self.matter_type.value
+        shape = np.array([
+            [[0, 0, 0],
+             [0, matter, 0],
+             [0, 0, 0]],
+            [[0, matter, 0],
+             [matter, matter, matter],
+             [0, matter, 0]],
+            [[0, 0, 0],
+             [0, matter, 0],
+             [0, 0, 0]],
+        ])
+
+        multiplier = tuple([int(self.dimension[i] / Light.default_dimension[i]) for i in range(len(self.dimension))])
+
+        shape = np.kron(shape, np.ones(multiplier))
+
+        return shape
