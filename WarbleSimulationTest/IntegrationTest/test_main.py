5acd2ccee0da76042f52c220e64909c92ac348cf
Yosef Saputra <yosef.saputra@gmail.com>
2019-02-28 20:20:08 -0600

    To move test_main to test_main_2. To add more entities in test_main

--- WarbleSimulationTest/IntegrationTest/test_main.py
+++ WarbleSimulationTest/IntegrationTest/test_main.py
@@ -5,11 +5,14 @@ from unittest import TestCase
 
 import WarbleSimulation.System.SpaceFactor as SpaceFactor
 from WarbleSimulation.System.Entity.Concrete.AirConditioner import AirConditioner
+from WarbleSimulation.System.Entity.Concrete.Chair import Chair
+from WarbleSimulation.System.Entity.Concrete.Human import Human
 from WarbleSimulation.System.Entity.Concrete.Light import Light
 from WarbleSimulation.System.Entity.Concrete.SmokeDetector import SmokeDetector
 from WarbleSimulation.System.Entity.Concrete.Table import Table
 from WarbleSimulation.System.Entity.Concrete.Thermostat import Thermostat
 from WarbleSimulation.System.Entity.Concrete.Wall import Wall
+from WarbleSimulation.System.Entity.Concrete.Wardrobe import Wardrobe
 from WarbleSimulation.System.System import System
 from WarbleSimulation.util import Logger, Plotter
 from WarbleSimulationTest import test_settings
@@ -26,26 +29,31 @@ class TestMain(TestCase):
         self.system = System('MyNewSystem')
 
         # Put Space on the System
-        self.system.put_space(dimension=(40, 30, 10), resolution=1,
+        self.system.put_space(dimension=(40, 30, 12), resolution=1,
                               space_factor_types=[i for i in SpaceFactor.SpaceFactor])
 
         # Put Entity on the Space
         table1 = Table(uuid=uuid.uuid4(), dimension_x=(2, 1, 1))
-        self.system.put_entity(table1, (0, 0, 0))
         light1 = Light(uuid=uuid.uuid4(), dimension_x=(1, 1, 1))
-        self.system.put_entity(light1, (19, 14, 7))
         ac1 = AirConditioner(uuid=uuid.uuid4())
-        self.system.put_entity(ac1, (37, 10, 7), unit_orientation=(-1, 0, 0))
         sd1 = SmokeDetector(uuid=uuid.uuid4())
-        self.system.put_entity(sd1, (0, 10, 8), unit_orientation=(1, 0, 0))
         thermostat1 = Thermostat(uuid=uuid.uuid4())
+        ch1 = Chair(uuid=uuid.uuid4())
+        h1 = Human(uuid=uuid.uuid4())
+        w1 = Wardrobe(uuid=uuid.uuid4())
+
+        self.system.put_entity(table1, (0, 0, 0))
+        self.system.put_entity(light1, (19, 14, 9))
+        self.system.put_entity(ac1, (37, 10, 9), unit_orientation=(-1, 0, 0))
+        self.system.put_entity(sd1, (10, 10, 11), unit_orientation=(0, 0, -1))
         self.system.put_entity(thermostat1, (30, 0, 5))
+        self.system.put_entity(ch1, (4, 4, 0), unit_orientation=(0, -1, 0))
+        self.system.put_entity(h1, (25, 20, 0), unit_orientation=(0, -1, 0))
+        self.system.put_entity(w1, (0, 20, 0), unit_orientation=(1, 0, 0))
 
         # Compare Space Factor Matter
         self.system.space.space_factors[SpaceFactor.SpaceFactor.MATTER][SpaceFactor.Matter.MATTER].tofile(
             os.path.join(test_settings.actual_path, test_name + '_space_matter.txt'))
-        self.assertTrue(filecmp.cmp(os.path.join(test_settings.expected_path, test_name + '_space_matter.txt'),
-                                    os.path.join(test_settings.actual_path, test_name + '_space_matter.txt')))
 
         # Plot
         Plotter.plot_scatter_3d(
@@ -75,6 +83,41 @@ class TestMain(TestCase):
         self.system.put_entity(wall_3, (0, 0.25, 0))
         wall_4 = Wall(uuid=uuid.uuid4(), dimension_x=(0.25, 9.5, 5))
         self.system.put_entity(wall_4, (19.75, 0.25, 0))
+
+        # Compare Space Factor Matter
+        self.system.space.space_factors[SpaceFactor.SpaceFactor.MATTER][SpaceFactor.Matter.MATTER].tofile(
+            os.path.join(test_settings.actual_path, test_name + '_space_matter.txt'))
+        self.assertTrue(filecmp.cmp(os.path.join(test_settings.expected_path, test_name + '_space_matter.txt'),
+                                    os.path.join(test_settings.actual_path, test_name + '_space_matter.txt')))
+
+        # Plot
+        Plotter.plot_scatter_3d(
+            array3d=self.system.space.space_factors[SpaceFactor.SpaceFactor.MATTER][SpaceFactor.Matter.MATTER],
+            zero_value=SpaceFactor.MatterType.ATMOSPHERE.value,
+            filename=os.path.join(test_settings.actual_path, test_name + '_plot.html'),
+            auto_open=test_settings.auto_open)
+
+    def test_main_2(self):
+        test_name = 'test_main'
+
+        # Create System
+        self.system = System('MyNewSystem')
+
+        # Put Space on the System
+        self.system.put_space(dimension=(40, 30, 10), resolution=1,
+                              space_factor_types=[i for i in SpaceFactor.SpaceFactor])
+
+        # Put Entity on the Space
+        table1 = Table(uuid=uuid.uuid4(), dimension_x=(2, 1, 1))
+        self.system.put_entity(table1, (0, 0, 0))
+        light1 = Light(uuid=uuid.uuid4(), dimension_x=(1, 1, 1))
+        self.system.put_entity(light1, (19, 14, 7))
+        ac1 = AirConditioner(uuid=uuid.uuid4())
+        self.system.put_entity(ac1, (37, 10, 7), unit_orientation=(-1, 0, 0))
+        sd1 = SmokeDetector(uuid=uuid.uuid4())
+        self.system.put_entity(sd1, (0, 10, 8), unit_orientation=(1, 0, 0))
+        thermostat1 = Thermostat(uuid=uuid.uuid4())
+        self.system.put_entity(thermostat1, (30, 0, 5))
 
         # Compare Space Factor Matter
         self.system.space.space_factors[SpaceFactor.SpaceFactor.MATTER][SpaceFactor.Matter.MATTER].tofile(
