34624ada4494187f19be8c929f4d2e80ff741df4
Yosef Saputra <yosef.saputra@gmail.com>
2019-03-20 14:09:09 -0500

    To merge PowerManagement.py and Power.py. To move ElectricPower() default in PowerInput and PowerOutput

--- WarbleSimulation/System/Entity/Basic/Power.py
+++ WarbleSimulation/System/Entity/Basic/Power.py
@@ -22,16 +22,34 @@ class ElectricPower(Power):
 class PowerInput:
     identifier = 'PowerInput'
 
-    def __init__(self, parent):
+    def __init__(self, parent, power=ElectricPower(voltage=0)):
         self.parent = parent
-        self.power = ElectricPower(voltage=0)
+        self.power = power
         self.power_wires = TypeList(PowerWire)
 
 
 class PowerOutput:
     identifier = 'PowerOutput'
 
-    def __init__(self, parent):
+    def __init__(self, parent, power=ElectricPower(voltage=0)):
         self.parent = parent
-        self.power = ElectricPower(voltage=0)
+        self.power = power
         self.power_wires = TypeList(PowerWire)
+
+
+class PowerManagement:
+    def __init__(self):
+        self.power_inputs = TypeList(PowerInput)
+        self.power_outputs = TypeList(PowerOutput)
+
+    def get_power_input(self, index=0):
+        if index < len(self.power_inputs):
+            return self.power_inputs[index]
+        else:
+            raise IndexError
+
+    def get_power_output(self, index=0):
+        if index < len(self.power_outputs):
+            return self.power_outputs[index]
+        else:
+            raise IndexError
