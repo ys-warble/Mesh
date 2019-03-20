e3231a7c83041c6a2c8d8065e780fad8bf35362a
Yosef Saputra <yosef.saputra@gmail.com>
2019-03-20 03:28:37 -0500

    To move the import statement inside constructor

--- WarbleSimulation/System/Entity/Channel/PowerWire.py
+++ WarbleSimulation/System/Entity/Channel/PowerWire.py
@@ -1,9 +1,10 @@
-from WarbleSimulation.System.Entity.Basic.Power import PowerInput, PowerOutput
 from WarbleSimulation.System.Entity.Channel import Channel
 
 
 class PowerWire(Channel):
     def __init__(self, power_output, power_input):
+        from WarbleSimulation.System.Entity.Basic.Power import PowerInput, PowerOutput
+
         super().__init__()
 
         if (not isinstance(power_input, PowerInput) or power_input is None) or \
@@ -12,3 +13,6 @@ class PowerWire(Channel):
 
         self.power_input = power_input
         self.power_output = power_output
+
+        self.power_input.power_wires.append(self)
+        self.power_output.power_wires.append(self)
