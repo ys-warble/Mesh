d3591d6b32e4fadc6b2c722c7f531c18a9d328db
Yosef Saputra <yosef.saputra@gmail.com>
2019-03-21 02:16:04 -0500

    To create the Pipes in Compute constructor. To define Compute constructor. To add handle_task() abstract method in Compute.

--- WarbleSimulation/System/Entity/Function/Compute.py
+++ WarbleSimulation/System/Entity/Function/Compute.py
@@ -1,6 +1,13 @@
-class Compute:
-    def __init__(self):
-        pass
+from multiprocessing import Pipe
 
-    def run(self, task_pipe):
+
+class Compute:
+    def __init__(self, entity):
+        self.entity = entity
+        self.p_task_pipe, self.c_task_pipe = Pipe()
+
+    def run(self):
+        raise NotImplementedError
+
+    def handle_task(self, task):
         raise NotImplementedError
