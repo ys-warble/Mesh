ae01008de3dc85a88891eb25eac8f3427aee841a
Yosef Saputra <yosef.saputra@gmail.com>
2019-03-21 19:52:36 -0500

    To add self.process in Compute.

--- WarbleSimulation/System/Entity/Function/Compute.py
+++ WarbleSimulation/System/Entity/Function/Compute.py
@@ -4,6 +4,7 @@ from multiprocessing import Pipe
 class Compute:
     def __init__(self, entity):
         self.entity = entity
+        self.process = None
         self.p_task_pipe, self.c_task_pipe = Pipe()
 
     def run(self):
