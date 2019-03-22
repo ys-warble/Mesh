9e56a44842c10f3ebcb25948208422587563af26
Yosef Saputra <yosef.saputra@gmail.com>
2019-03-22 17:54:03 -0500

    To make value optional in TaskResponse

--- WarbleSimulation/System/Entity/Function/Tasked.py
+++ WarbleSimulation/System/Entity/Function/Tasked.py
@@ -54,7 +54,7 @@ class Task(BaseTask):
 
 
 class TaskResponse:
-    def __init__(self, status, value):
+    def __init__(self, status, value=None):
         self.status = status
         self.value = value
 
