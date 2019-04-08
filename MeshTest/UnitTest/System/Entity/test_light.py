8e5cf229887b6827ed32a11fefa16a8777e7a840
Yosef Saputra <yosef.saputra@gmail.com>
2019-04-07 21:47:23 -0500

    To split test_light to 3 different classes, to differentiate among Light with Powered, Tasked, and Compute (optional).

--- MeshTest/UnitTest/System/Entity/test_light.py
+++ MeshTest/UnitTest/System/Entity/test_light.py
@@ -3,24 +3,24 @@ import uuid
 import numpy as np
 
 from Mesh.System.Entity.Concrete.Light import Light
-from Mesh.System.Entity.Function import Function
+from Mesh.System.Entity.Function import Function, FunctionUnsupportedError
 from Mesh.System.Entity.Function.Powered import ElectricPower
-from Mesh.System.Entity.Function.Tasked import TaskName, Status, SystemTask, Task, TaskResponse
+from Mesh.System.Entity.Function.Tasked import TaskName, SystemTask, Task, ProgramTask, TaskResponse, Status
 from Mesh.System.SpaceFactor import MatterType
 from MeshTest import test_settings
 from MeshTest.AppTestCase import AppTestCase
 
 
-class TestLight(AppTestCase):
+class TestLightPowered(AppTestCase):
     def setUp(self):
         super().setUp()
+        self.light = Light(uuid=uuid.uuid4(), selected_functions=(Function.POWERED,))
 
     def tearDown(self):
         self.logger.info(test_settings.end_title_format(self._testMethodName))
         self.logger.info('')
 
     def test_get_default_shape(self):
-        self.light = Light(uuid=uuid.uuid4())
         self.assertEqual(self.light.get_default_shape().shape, (3, 3, 3))
         matter = MatterType.GLASS.value
         np.testing.assert_array_equal(self.light.get_default_shape(),
@@ -34,74 +34,51 @@ class TestLight(AppTestCase):
                                         [0, matter, 0],
                                         [0, 0, 0]]])
 
-        self.light.destroy()
-
     def test_send_task(self):
-        self.light = Light(uuid=uuid.uuid4())
-
         # SystemTask
-        self.light.send_task(SystemTask(name=TaskName.ACTIVE))
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.send_task(SystemTask(name=TaskName.ACTIVE)))
 
         # EntityTask
-        self.light.send_task(Task(name=TaskName.GET_INFO))
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.send_task(Task(name=TaskName.GET_INFO)))
 
-        self.light.destroy()
+        # ProgramTask
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.send_task(ProgramTask(name=TaskName.END)))
 
     def test_recv_task_resp(self):
-        self.light = Light(uuid=uuid.uuid4())
-        self.light.send_task(SystemTask(name=TaskName.SET_POWER, value={'power': ElectricPower(110)}))
-        self.assertEqual(self.light.recv_task_resp(),
-                         TaskResponse(status=Status.OK, value=None))
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.send_task(
+            SystemTask(name=TaskName.SET_POWER, value={'power': ElectricPower(110)})))
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.recv_task_resp())
 
         # SystemTask
-        self.light.send_task(SystemTask(name=TaskName.ACTIVE))
-        self.assertEqual(self.light.recv_task_resp(),
-                         TaskResponse(status=Status.OK, value=None))
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.send_task(SystemTask(name=TaskName.ACTIVE)))
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.recv_task_resp())
 
         # EntityTask
-        self.light.send_task(Task(name=TaskName.GET_INFO))
-        self.assertEqual(self.light.recv_task_resp(),
-                         TaskResponse(status=Status.OK, value={'info': {
-                             'uuid': str(self.light.uuid),
-                             'identifier': type(self.light).identifier,
-                             'type': {
-                                 'actuator': [
-                                     'POWER'
-                                 ],
-                                 'sensor': [],
-                                 'accessor': []
-                             },
-                         }}))
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.send_task(Task(name=TaskName.GET_INFO)))
+        self.assertRaises(FunctionUnsupportedError, lambda: self.light.recv_task_resp())
 
-        self.light.destroy()
 
-    def test_task_no_compute(self):
+class TestLightTasked(TestLightPowered):
+    def setUp(self):
+        super().setUp()
         self.light = Light(uuid=uuid.uuid4(), selected_functions=(Function.POWERED, Function.TASKED))
 
-        self.light.send_task(SystemTask(name=TaskName.SET_POWER, value={'power': ElectricPower(110)}))
-        self.assertEqual(self.light.recv_task_resp(),
-                         TaskResponse(status=Status.OK, value=None))
-
+    def test_send_task(self):
         # SystemTask
-        self.light.send_task(SystemTask(name=TaskName.DEACTIVATE))
-        self.assertFalse(self.light.active)
-        self.assertEqual(self.light.recv_task_resp(),
-                         TaskResponse(status=Status.OK, value=None))
-
         self.light.send_task(SystemTask(name=TaskName.ACTIVE))
-        self.assertTrue(self.light.active)
-        self.assertEqual(self.light.recv_task_resp(),
-                         TaskResponse(status=Status.OK, value=None))
-
-        self.light.send_task(SystemTask(name=TaskName.DEACTIVATE))
-        self.assertFalse(self.light.active)
-        self.assertEqual(self.light.recv_task_resp(),
-                         TaskResponse(status=Status.OK, value=None))
 
         # EntityTask
         self.light.send_task(Task(name=TaskName.GET_INFO))
-        self.assertEqual(self.light.recv_task_resp(),
-                         TaskResponse(status=Status.OK, value={'info': {
+
+    def test_recv_task_resp(self):
+        self.light.send_task(SystemTask(name=TaskName.SET_POWER, value={'power': ElectricPower(110)}))
+        self.assertEqual(TaskResponse(Status.OK), self.light.recv_task_resp())
+
+        self.light.send_task(SystemTask(name=TaskName.ACTIVE))
+        self.assertEqual(TaskResponse(Status.OK), self.light.recv_task_resp())
+
+        self.light.send_task(Task(name=TaskName.GET_INFO))
+        self.assertEqual(TaskResponse(status=Status.OK, value={'info': {
                              'uuid': str(self.light.uuid),
                              'identifier': type(self.light).identifier,
                              'type': {
@@ -111,18 +88,14 @@ class TestLight(AppTestCase):
                                  'sensor': [],
                                  'accessor': []
                              },
-                         }}))
+        }}),
+                         self.light.recv_task_resp())
 
-        self.light.destroy()
-
-    def test_task_with_compute(self):
-        self.light = Light(uuid=uuid.uuid4())
-
+    def test_active_deactivate(self):
         self.light.send_task(SystemTask(name=TaskName.SET_POWER, value={'power': ElectricPower(110)}))
         self.assertEqual(self.light.recv_task_resp(),
                          TaskResponse(status=Status.OK, value=None))
 
-        # SystemTask
         self.light.send_task(SystemTask(name=TaskName.DEACTIVATE))
         self.assertEqual(self.light.recv_task_resp(),
                          TaskResponse(status=Status.OK, value=None))
@@ -143,7 +116,6 @@ class TestLight(AppTestCase):
         self.light.send_task(SystemTask(name=TaskName.GET_SYSTEM_INFO))
         self.assertFalse(self.light.recv_task_resp().value['system_info']['active'])
 
-        # EntityTask
         self.light.send_task(Task(name=TaskName.GET_INFO))
         self.assertEqual(self.light.recv_task_resp(),
                          TaskResponse(status=Status.OK, value={'info': {
@@ -161,7 +133,6 @@ class TestLight(AppTestCase):
         self.light.send_task(SystemTask(name=TaskName.GET_SYSTEM_INFO))
         self.assertFalse(self.light.recv_task_resp().value['system_info']['active'])
 
-        # SystemTask
         self.light.send_task(SystemTask(name=TaskName.DEACTIVATE))
         self.assertEqual(self.light.recv_task_resp(),
                          TaskResponse(status=Status.OK, value=None))
@@ -176,7 +147,6 @@ class TestLight(AppTestCase):
         self.light.send_task(SystemTask(name=TaskName.GET_SYSTEM_INFO))
         self.assertTrue(self.light.recv_task_resp().value['system_info']['active'])
 
-        # EntityTask
         self.light.send_task(Task(name=TaskName.GET_INFO))
         self.assertEqual(self.light.recv_task_resp(),
                          TaskResponse(status=Status.OK, value={'info': {
@@ -191,9 +161,7 @@ class TestLight(AppTestCase):
                              },
                          }}))
 
-        self.light.destroy()
-
-    def test_light_1(self):
+    def test_input_power(self):
         def check_active(l_light, expected):
             if expected:
                 self.assertTrue(l_light.active)
@@ -209,9 +177,7 @@ class TestLight(AppTestCase):
         light.get_function(Function.POWERED).get_power_input().set_power(ElectricPower(220))
         check_active(light, False)
 
-        light.destroy()
-
-    def test_light_2(self):
+    def test_input_power_autodeactivate(self):
         def check_active(l_light, expected):
             if expected:
                 self.assertTrue(l_light.active)
@@ -246,42 +212,12 @@ class TestLight(AppTestCase):
         light.recv_task_resp()
         check_active(light, False)
 
-        light.destroy()
 
-    def test_light_3(self):
-        def check_active(l_light, expected):
-            if expected:
-                l_light.send_task(SystemTask(TaskName.GET_SYSTEM_INFO))
-                self.assertTrue(l_light.recv_task_resp().value['system_info']['active'])
-            else:
-                l_light.send_task(SystemTask(TaskName.GET_SYSTEM_INFO))
-                self.assertFalse(l_light.recv_task_resp().value['system_info']['active'])
+class TestLightCompute(TestLightTasked):
+    def setUp(self):
+        super().setUp()
+        self.light = Light(uuid=uuid.uuid4(), selected_functions=(Function.POWERED, Function.TASKED, Function.COMPUTE))
 
-        light = Light(uuid=uuid.uuid4(), selected_functions=(Function.POWERED, Function.TASKED, Function.COMPUTE))
-        check_active(light, False)
-
-        light.send_task(SystemTask(TaskName.ACTIVE))
-        light.recv_task_resp()
-        check_active(light, False)
-
-        light.send_task(SystemTask(TaskName.SET_POWER, value={'power': ElectricPower(110)}))
-        light.recv_task_resp()
-        light.send_task(SystemTask(TaskName.ACTIVE))
-        light.recv_task_resp()
-        check_active(light, True)
-
-        light.send_task(SystemTask(TaskName.SET_POWER, value={'power': ElectricPower(0)}))
-        light.recv_task_resp()
-        check_active(light, False)
-        light.send_task(SystemTask(TaskName.ACTIVE))
-        light.recv_task_resp()
-        check_active(light, False)
-
-        light.send_task(SystemTask(TaskName.SET_POWER, value={'power': ElectricPower(220)}))
-        light.recv_task_resp()
-        check_active(light, False)
-        light.send_task(SystemTask(TaskName.ACTIVE))
-        light.recv_task_resp()
-        check_active(light, False)
-
-        light.destroy()
+    def tearDown(self):
+        self.light.destroy()
+        super().tearDown()
